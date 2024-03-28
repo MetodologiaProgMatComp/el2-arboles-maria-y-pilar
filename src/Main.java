647ad0dba63e4f94412da543a9d79617d3e1a49c
mariadelaorden <mariadelaorden2005@hotmail.com>
2024-03-28 20:35:16 +0100

    Preguntas arbol binario

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import es.uah.ed.arbol.ArbolBinarioDeBusqueda;
 import es.uah.ed.arbol.Nodo;
+import es.uah.listas.ListadoblementeEnlazada;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,6 +21,25 @@ public class Main {
 
         System.out.println("Contenido del subárbol derecho: ");
         imprimirInOrden(subArbolDerecha.getRaiz());
+
+        System.out.println("Grado del árbol:"+ arbol.getGrado());
+        System.out.println("Altura del árbol:"+ arbol.getAltura());
+
+        ListadoblementeEnlazada<Integer> lista = arbol.getListaDatosNivel(2);
+
+        System.out.println("Datos nivel 2:");
+        for (int i = 0; i < lista.getNumeroElementos(); i++) {
+            System.out.println(lista.getElemento(i));
+        }
+        System.out.println("Arbol homogéneo:"+arbol.isArbolHomogeneo());
+        System.out.println("Arbol completo:"+arbol.isArbolCompleto());
+        System.out.println("Arbol casi completo:"+arbol.isArbolCasiCompleto());
+
+        ListadoblementeEnlazada<Integer> camino = arbol.getCamino(2);
+        System.out.println("Obtener camino hasta el elemento 2:");
+        for (int i = 0; i < camino.getNumeroElementos(); i++) {
+            System.out.println(camino.getElemento(i));
+        }
     }
     
     private static void imprimirInOrden (Nodo nodo) {
@@ -29,5 +49,7 @@ public class Main {
             imprimirInOrden(nodo.getDerecha());
         }
     }
+
+
     
 }
--- src/es/uah/ed/arbol/ArbolBinarioDeBusqueda.java
+++ src/es/uah/ed/arbol/ArbolBinarioDeBusqueda.java
@@ -1,5 +1,7 @@
 package es.uah.ed.arbol;
 
+import es.uah.listas.ListadoblementeEnlazada;
+
 public class ArbolBinarioDeBusqueda {
     private Nodo raiz=null;
 
@@ -56,4 +58,115 @@ public class ArbolBinarioDeBusqueda {
     public Nodo getRaiz() {
         return raiz;
     }
+
+    public int getGrado() {
+        return getGradoRecursivo(raiz);
+    }
+
+    private int getGradoRecursivo(Nodo nodo) {
+        if (nodo == null) {
+            return 0;
+        }
+        int gradoIzquierda = getGradoRecursivo(nodo.getIzquierda());
+        int gradoDerecha = getGradoRecursivo(nodo.getDerecha());
+        return Math.max(gradoIzquierda, gradoDerecha) + 1;
+
+    }
+
+    public int getAltura() {
+        return getAlturaRecursivo(raiz);
+    }
+
+    private int getAlturaRecursivo(Nodo nodo) {
+        if (nodo == null) {
+            return 0;
+        }
+        int alturaIzquierda = getAlturaRecursivo(nodo.getIzquierda());
+        int alturaDerecha = getAlturaRecursivo(nodo.getDerecha());
+        return Math.max(alturaIzquierda, alturaDerecha) + 1;
+
+    }
+
+    public ListadoblementeEnlazada<Integer> getListaDatosNivel(int nivel) {
+        ListadoblementeEnlazada<Integer> datosnivel = new ListadoblementeEnlazada<>();
+        getDatosNivelRecursivo(raiz, nivel, datosnivel, 1);
+        return datosnivel;
+    }
+
+    private void getDatosNivelRecursivo(Nodo nodo, int nivel, ListadoblementeEnlazada<Integer> datosnivel, int nivelActual) {
+        if (nodo == null) {
+            return;
+        }
+        if (nivelActual == nivel) {
+            datosnivel.add((Integer) nodo.getData());
+        } else {
+            getDatosNivelRecursivo(nodo.getIzquierda(), nivel, datosnivel, nivelActual + 1);
+            getDatosNivelRecursivo(nodo.getDerecha(), nivel, datosnivel, nivelActual + 1);
+        }
+    }
+
+    public boolean isArbolHomogeneo() {
+        return isArbolHomogeneoRecursivo(raiz, getGrado());
+    }
+
+    private boolean isArbolHomogeneoRecursivo(Nodo nodo, int grado) {
+        if (nodo == null) {
+            return true;
+        }
+        if ((nodo.getIzquierda() != null && nodo.getDerecha() == null) ||
+                (nodo.getIzquierda() == null && nodo.getDerecha() != null) ||
+                getGradoRecursivo(nodo) != grado) {
+            return false;
+        }
+        return isArbolHomogeneoRecursivo(nodo.getIzquierda(), grado) && isArbolHomogeneoRecursivo(nodo.getDerecha(), grado);
+    }
+
+    public boolean isArbolCompleto() {
+        int altura = getAltura();
+        for (int i = 1; i < altura-1; i++) {
+            if (getListaDatosNivel(i).getNumeroElementos() != Math.pow(2, i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isArbolCasiCompleto() {
+        int altura = getAltura();
+        for (int i = 1; i < altura - 1; i++) {
+            if (getListaDatosNivel(i).getNumeroElementos() != Math.pow(2, i - 1)) {
+                return false;
+            }
+        }
+        ListadoblementeEnlazada<Integer> datosUltimoNivel = getListaDatosNivel(altura);
+        int nodosUltimoNivel = datosUltimoNivel.getNumeroElementos();
+        int nodosesperados = 1;
+        while (nodosesperados < nodosUltimoNivel) {
+            nodosesperados *= 2;
+        }
+        return nodosesperados == nodosUltimoNivel;
+    }
+    public ListadoblementeEnlazada<Integer> getCamino(Integer elemento) {
+        ListadoblementeEnlazada<Integer> camino = new ListadoblementeEnlazada<>();
+        obtenerCamino(raiz, elemento, camino);
+        return camino;
+    }
+
+    private boolean obtenerCamino(Nodo nodo, Integer elemento, ListadoblementeEnlazada<Integer> camino) {
+        if (nodo == null) {
+            return false;
+        }
+        camino.add((Integer) nodo.getData());
+        if (nodo.getData().equals(elemento)) {
+            return true;
+        }
+        if (obtenerCamino(nodo.getIzquierda(), elemento, camino) || obtenerCamino(nodo.getDerecha(), elemento, camino)) {
+            return true;
+        }
+        camino.del(camino.getNumeroElementos());  //Elimino del final
+        return false;
+    }
+
+
+
 }
